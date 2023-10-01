8a4415cf5b8e6394c3ee2afae07e8f0eaf1ca80b
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-10-01 04:10:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addition:
+const additionResult = 4 + 5;
+console.log("additionResult: " + additionResult);
+//Subtraction:
+const subtractionResult = 4 - 5;
+console.log("subtractionResult: " + subtractionResult);
+//Multiplication:
+const multiplicationResult = 4 * 6;
+console.log("multiplicationResult: " + multiplicationResult);
+//Division:
+const divisionResult = 4 / 2;
+console.log("divisionResult: " + divisionResult);
